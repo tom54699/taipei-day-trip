376038b773a2b02ddfe7c3dd28e7fcaa3f14ff70
Tom <tom54699@gmail.com>
2022-11-26 15:41:27 +0800

    Fix: 把圖片的點改成幾張圖幾個點 11/26

--- flask/api/static/js/attractionPage.js
+++ flask/api/static/js/attractionPage.js
@@ -16,7 +16,7 @@ export async function generateAttraction(id){
         console.log(data)
         
         imageLength = data["data"]["images"].length
-        for(let i=0;i<3;i++){
+        for(let i=0;i<imageLength;i++){
             let newFetchImg = data["data"]["images"][i]
             fetchImg.push(newFetchImg)
         }
@@ -29,6 +29,7 @@ export async function generateAttraction(id){
 
  
         /* Define Node */
+        let dotBoxNode=document.getElementsByClassName("dotBox")
         let attractionNameNode = document.getElementsByClassName("attractionName")
         let attractionImgNode = document.getElementsByClassName("attractionImg")
         let attractionCategoryNode = document.getElementsByClassName("attractionCategory")
@@ -43,6 +44,13 @@ export async function generateAttraction(id){
         attractionDescriptionNode[0].textContent = fetchDescription
         attractionAddressNode[0].textContent = fetchAddress
         attractionTransportNode[0].textContent = fetchTransport
+
+        /* Add Dot */
+        for(let i=0;i<imageLength;i++){
+            let dot = document.createElement("section")
+            dot.setAttribute("class","dot")
+            dotBoxNode[0].appendChild(dot)
+        }
     })
 }
 
@@ -64,38 +72,27 @@ window.addEventListener("DOMContentLoaded", async() => {
     let attractionId = getNumberAtEnd(pathname)
     await generateAttraction(attractionId)
     imgId = 0
-    dot1.style.backgroundImage = "url(/static/pic/circle_current.png)"
+    dot[imgId].style.backgroundImage = "url(/static/pic/circle_current.png)"
 })
 
 /* Img Button */
 let leftArrow = document.getElementById("leftArrow");
 let rightArrow = document.getElementById("rightArrow");
-let dot1 = document.getElementById("dot1");
-let dot2= document.getElementById("dot2");
-let dot3 = document.getElementById("dot3");
+let dot = document.getElementsByClassName("dot")
 
 rightArrow.addEventListener("click", () => {
     let attractionImgNode = document.getElementsByClassName("attractionImg")
     imgId = imgId +1
-    if(imgId >= 3){
+    if(imgId >= imageLength){
         imgId = 0
     }
-    switch(true){
-        case(imgId == 0):
-        dot1.style.backgroundImage = "url(/static/pic/circle_current.png)"
-        dot2.style.backgroundImage = "url(/static/pic/circle.png)"
-        dot3.style.backgroundImage = "url(/static/pic/circle.png)"
-        break;
-        case(imgId == 1):
-        dot1.style.backgroundImage = "url(/static/pic/circle.png)"
-        dot2.style.backgroundImage = "url(/static/pic/circle_current.png)"
-        dot3.style.backgroundImage = "url(/static/pic/circle.png)"
-        break;
-        case(imgId == 2):
-        dot1.style.backgroundImage = "url(/static/pic/circle.png)"
-        dot2.style.backgroundImage = "url(/static/pic/circle.png)"
-        dot3.style.backgroundImage = "url(/static/pic/circle_current.png)"
-        break;
+    if(imgId == 0){
+        dot[imageLength-1].style.backgroundImage = "url(/static/pic/circle.png)"
+        dot[imgId].style.backgroundImage = "url(/static/pic/circle_current.png)" 
+
+    }else{
+        dot[imgId-1].style.backgroundImage = "url(/static/pic/circle.png)"
+        dot[imgId].style.backgroundImage = "url(/static/pic/circle_current.png)" 
     }
     console.log(imgId)
     attractionImgNode[0].style.backgroundImage = `url(${fetchImg[imgId]})`
@@ -105,24 +102,15 @@ leftArrow.addEventListener("click", () => {
     let attractionImgNode = document.getElementsByClassName("attractionImg")
     imgId = imgId -1
     if(imgId < 0){
-        imgId = 3-1
+        imgId = imageLength-1
     }
-    switch(true){
-        case(imgId == 0):
-        dot1.style.backgroundImage = "url(/static/pic/circle_current.png)"
-        dot2.style.backgroundImage = "url(/static/pic/circle.png)"
-        dot3.style.backgroundImage = "url(/static/pic/circle.png)"
-        break;
-        case(imgId == 1):
-        dot1.style.backgroundImage = "url(/static/pic/circle.png)"
-        dot2.style.backgroundImage = "url(/static/pic/circle_current.png)"
-        dot3.style.backgroundImage = "url(/static/pic/circle.png)"
-        break;
-        case(imgId == 2):
-        dot1.style.backgroundImage = "url(/static/pic/circle.png)"
-        dot2.style.backgroundImage = "url(/static/pic/circle.png)"
-        dot3.style.backgroundImage = "url(/static/pic/circle_current.png)"
-        break;
+    if(imgId == imageLength-1){
+        dot[0].style.backgroundImage = "url(/static/pic/circle.png)"
+        dot[imgId].style.backgroundImage = "url(/static/pic/circle_current.png)" 
+
+    }else{
+        dot[imgId+1].style.backgroundImage = "url(/static/pic/circle.png)"
+        dot[imgId].style.backgroundImage = "url(/static/pic/circle_current.png)" 
     }
     console.log(imgId)
     attractionImgNode[0].style.backgroundImage = `url(${fetchImg[imgId]})`
