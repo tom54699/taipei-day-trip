3fb846e251491d6438aecd2fcfb18627d271634e
Tom <tom54699@gmail.com>
2022-11-29 17:13:38 +0800

    Fix: 資料庫設定  11/29

--- mysql/taipei_attractions.sql
+++ mysql/taipei_attractions.sql
@@ -108,7 +108,7 @@ CREATE TABLE `member` (
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,6 +117,7 @@ CREATE TABLE `member` (
 
 LOCK TABLES `member` WRITE;
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'test','test@test.com','$2b$10$BzyvP8A0n1rkDibMYuvZmey5pfesJFiLOLwsOAZF4dL1DmqESK8dm');
 /*!40000 ALTER TABLE `member` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -129,4 +130,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-29 16:27:51
+-- Dump completed on 2022-11-29 17:12:02
